54de6372bcb6572e33063b7d883980c371415faf
t0kage3 <t0kage3@t0kage3.net>
2018-07-31 10:11:00 +0900

    Add servo port 7,8.

--- src/tpot-servo/tpot-servo.ino
+++ src/tpot-servo/tpot-servo.ino
@@ -12,14 +12,18 @@
 #define PIN_SERVO_4 5
 #define PIN_SERVO_5 6
 #define PIN_SERVO_6 7
+#define PIN_SERVO_7 8
+#define PIN_SERVO_8 9
 
-Servo Servo1,Servo2,Servo3,Servo4,Servo5,Servo6;
+Servo Servo1,Servo2,Servo3,Servo4,Servo5,Servo6,Servo7,Servo8;
 TURIPport* PortServo1;
 TURIPport* PortServo2;
 TURIPport* PortServo3;
 TURIPport* PortServo4;
 TURIPport* PortServo5;
 TURIPport* PortServo6;
+TURIPport* PortServo7;
+TURIPport* PortServo8;
 
 int ServoDeg[6];
 
@@ -33,14 +37,18 @@ void setup() {
   Servo4.attach(PIN_SERVO_4);
   Servo5.attach(PIN_SERVO_5);
   Servo6.attach(PIN_SERVO_6);
+  Servo5.attach(PIN_SERVO_7);
+  Servo6.attach(PIN_SERVO_8);
   Servo1.write(ServoDeg[0]);
   Servo2.write(ServoDeg[1]);
   Servo3.write(ServoDeg[2]);
   Servo4.write(ServoDeg[3]);
   Servo5.write(ServoDeg[4]);
   Servo6.write(ServoDeg[5]);
+  Servo7.write(ServoDeg[6]);
+  Servo8.write(ServoDeg[7]);
 
-  
+
 
   TURIPserver.begin(TURIP_MODEL, TURIP_SERIAL);
   TURIPserverSPI.begin();
@@ -50,51 +58,67 @@ void setup() {
   PortServo4 = TURIPserver.newPort(4);
   PortServo5 = TURIPserver.newPort(5);
   PortServo6 = TURIPserver.newPort(6);
-  
+  PortServo7 = TURIPserver.newPort(7);
+  PortServo8 = TURIPserver.newPort(8);
+
   PortServo1->postReceiveFunc = writeServo1;
   PortServo2->postReceiveFunc = writeServo2;
   PortServo3->postReceiveFunc = writeServo3;
   PortServo4->postReceiveFunc = writeServo4;
   PortServo5->postReceiveFunc = writeServo5;
   PortServo6->postReceiveFunc = writeServo6;
+  PortServo7->postReceiveFunc = writeServo7;
+  PortServo8->postReceiveFunc = writeServo8;
 }
 
 void loop() {
   TURIPserverSPI.update();
 }
 
-int writeServo1(){
+void writeServo1(){
   ServoDeg[0] = constrain(PortServo1->readInt16(), 0, 180);
   PortServo1->writeInt16(ServoDeg[0]);
   Servo1.write(ServoDeg[0]);
 }
 
-int writeServo2(){
+void writeServo2(){
   ServoDeg[1] = constrain(PortServo2->readInt16(), 0, 180);
   PortServo2->writeInt16(ServoDeg[1]);
   Servo2.write(ServoDeg[1]);
 }
 
-int writeServo3(){
+void writeServo3(){
   ServoDeg[2] = constrain(PortServo3->readInt16(), 0, 180);
   PortServo3->writeInt16(ServoDeg[2]);
   Servo3.write(ServoDeg[2]);
 }
 
-int writeServo4(){
+void writeServo4(){
   ServoDeg[3] = constrain(PortServo4->readInt16(), 0, 180);
   PortServo4->writeInt16(ServoDeg[3]);
   Servo4.write(ServoDeg[3]);
 }
 
-int writeServo5(){
+void writeServo5(){
   ServoDeg[4] = constrain(PortServo5->readInt16(), 0, 180);
   PortServo5->writeInt16(ServoDeg[4]);
   Servo5.write(ServoDeg[4]);
 }
 
-int writeServo6(){
+void writeServo6(){
   ServoDeg[5] = constrain(PortServo6->readInt16(), 0, 180);
   PortServo6->writeInt16(ServoDeg[5]);
   Servo6.write(ServoDeg[5]);
+}
+
+void writeServo7(){
+  ServoDeg[6] = constrain(PortServo6->readInt16(), 0, 180);
+  PortServo6->writeInt16(ServoDeg[6]);
+  Servo6.write(ServoDeg[6]);
+}
+
+void writeServo8(){
+  ServoDeg[7] = constrain(PortServo6->readInt16(), 0, 180);
+  PortServo6->writeInt16(ServoDeg[7]);
+  Servo6.write(ServoDeg[7]);
 }
